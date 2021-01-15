8026ab61658db74892e27fb03f9955bd10402647
kennishakapur <72257129+kennishakapur@users.noreply.github.com>
2021-01-15 17:34:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var database;
+var gameState=0;
+var playerCount;
+var form,player,game;
+var allPlayers;
+var car1,car2,car3,car4,cars;
+
+function setup(){
+    
+database=firebase.database();
+
+
+    createCanvas(displayWidth,displayHeight);
+
+    game = new Game();
+    game.getState();
+    game.start();
+   
+}
+
+function draw(){
+    background("white");
+   
+    if(playerCount==4){
+        game.update(1)
+        
+    }
+
+    if(gameState==1){
+        game.play()
+    }
+}
+
+
